756adbfb1868506e6d06f273f6514c0e280c4a83
Bryan Cusme <bryan.cusme@devoteam.com>
2023-01-13 09:49:16 +0100

    add db.js to deploy

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'dpg-cen0dfun6mpkfa76o660-a.frankfurt-postgres.render.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'afstore'),
+        user: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'kvTMIzJ0dW622XAqSI7U1XPchgVCsN9G'),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  });
