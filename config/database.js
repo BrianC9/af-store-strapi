2c133f3d4f284b9800c03938903872fc1b3c0bb5
Bryan Cusme <bryan.cusme@devoteam.com>
2023-01-13 09:57:12 +0100

    modify env vars

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+const PG_PW = process.env.PG_PASSWORD
+const PG_US= process.env.PG_USER
 module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
@@ -5,8 +7,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'dpg-cen0dfun6mpkfa76o660-a.frankfurt-postgres.render.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'afstore'),
-        user: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'kvTMIzJ0dW622XAqSI7U1XPchgVCsN9G'),
+        user: env('DATABASE_USERNAME', PG_US),
+        password: env('DATABASE_PASSWORD', PG_PW),
         ssl: env.bool('DATABASE_SSL', true),
       },
     },
